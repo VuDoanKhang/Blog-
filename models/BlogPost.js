157ec0df26a58cfacaca08fad3815b01fcf0f043
khang <vukhaq2910@gmail.com>
2024-04-09 02:33:05 +0700

    add samplepost, index.ejs and update code index.js,Blogpost.js post.ejs

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const BlogPostSchema = new Schema({
     title: String,
-    body: String
+    body: String,
+    username: String,
+    datePosted: { /* can declare property type with an object like this becau
+se we need 'default' */
+        type: Date,
+        default: new Date()
+    }
 })
 const BlogPost = mongoose.model('BlogPost', BlogPostSchema)
 module.exports = BlogPost
