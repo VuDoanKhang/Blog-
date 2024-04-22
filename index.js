538ad9119c50cef8824b0122bb3988d41f700b5d
khang <vukhaq2910@gmail.com>
2024-04-22 12:14:46 +0700

    Logout, 404 pages

--- index.js
+++ index.js
@@ -5,13 +5,25 @@ const path = require("path");
 const app = new express();
 //Khai báo express-fileupload
 const fileupload = require("express-fileupload");
-const expressSession = require('express-session');
-const authMiddleware = require('./middleware/authMiddleware')
+const expressSession = require("express-session");
+const authMiddleware = require("./middleware/authMiddleware");
+const redirectIfAuthenticatedMiddleware = require("./middleware/redirectIfAuthenticatedMiddleware");
+const logoutController = require("./controllers/logout");
 
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: true,
+    secret: "keyboard cat",
+  })
+);
 
-app.use(expressSession({
-  secret: 'keyboard cat'
-}))
+global.loggedIn = null;
+app.use("*", (req, res, next) => {
+  loggedIn = req.session.userId;
+  next();
+});
+
 app.use(fileupload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,27 +40,37 @@ mongoose.connect("mongodb://localhost:27017/").then(() => {
 });
 
 const newUserController = require("./controllers/newUser");
-app.get("/auth/register", newUserController);
+app.get("/auth/register", redirectIfAuthenticatedMiddleware, newUserController);
 const storeUserController = require("./controllers/storeUser");
-app.post("/users/register", storeUserController);
+app.post(
+  "/users/register",
+  redirectIfAuthenticatedMiddleware,
+  storeUserController
+);
 
 const loginController = require("./controllers/login");
-app.get("/auth/login", loginController);
+app.get("/auth/login", redirectIfAuthenticatedMiddleware, loginController);
 const loginUserController = require("./controllers/loginUser");
-app.post("/users/login", loginUserController);
+app.post(
+  "/users/login",
+  redirectIfAuthenticatedMiddleware,
+  loginUserController
+);
+
+app.get("/auth/logout", logoutController);
 
 // ---------------------------------------- check-----------------------
 const BlogPost = require("./models/BlogPost");
 // Tạo danh sách bài đăng
-app.get("/posts/new", async (req, res) => {
-  try {
-    const blogposts = await BlogPost.find({}); // Lấy danh sách các bài đăng
-    res.render("create", { blogposts: blogposts }); // Truyền danh sách bài đăng vào template
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
-  }
-});
+// app.get("/posts/new", async (req, res) => {
+//   try {
+//     const blogposts = await BlogPost.find({}); // Lấy danh sách các bài đăng
+//     res.render("create", { blogposts: blogposts }); // Truyền danh sách bài đăng vào template
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 // //-------------------------------------------x---------------------------------
 
 const getPostController = require("./controllers/getPost");
@@ -60,7 +82,7 @@ app.get("/", homeController);
 
 //-----------------------------------
 const newPostController = require("./controllers/newPost");
-app.get('/posts/new', authMiddleware, newPostController)
+app.get("/posts/new", authMiddleware, newPostController);
 
 const storePostController = require("./controllers/storePost");
 app.post("/posts/store", authMiddleware, storePostController);
@@ -76,6 +98,8 @@ app.get("/contact", (req, res) => {
 app.get("/samplepost", (req, res) => {
   res.render("samplepost");
 });
+
+app.use((req, res) => res.render("notfound"));
 
 //-------------------------------------\
 
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,12 +1,14 @@
-const User = require('../models/User')
+const User = require("../models/User");
 
 module.exports = async (req, res, next) => {
-    try {
-        const user = await User.findById(req.session.userId);
-        if (!user)
-            return res.redirect('/')
-        next()
-    } catch (err) {
-        console.log(err);
-    }
-}
+  try {
+    console.log("sdfdfdfsdfsdf");
+    const user = await User.findById(req.session.userId);
+    console.log({ dsfdsf: user });
+    if (!user) return res.redirect("/");
+    next();
+  } catch (err) {
+    console.log(err);
+    return res.redirect("/");
+  }
+};
